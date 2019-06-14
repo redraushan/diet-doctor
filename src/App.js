64b6cc4dbe9add48f7d9c2c7a8cd88f30e49cfc6
Raushan Sharma <redraushan@gmail.com>
2019-06-15 03:08:09 +0400

    Routes added: root, /login and /register

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-import React from 'react';
-import { Router, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { PrivateRoute } from './PrivateRoute.js';
-import { history } from './helpers';
-import { alertActions } from './actions';
-import { HomePage } from './components/HomePage';
-import { LoginPage } from './components/LoginPage';
-import { RegisterPage } from './components/RegisterPage';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { PrivateRoute } from "./PrivateRoute.js";
+import { history } from "./helpers";
+import { alertActions } from "./actions";
+import { HomePage } from "./components/HomePage";
+import { LoginPage } from "./components/LoginPage";
+import { RegisterPage } from "./components/RegisterPage";
 
 export class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        const { dispatch } = this.props;
-        history.listen((location, action) => {
-        });
-    }
+    const { dispatch } = this.props;
+    history.listen((location, action) => {});
+  }
 
-    render() {
-        const { alert } = this.props;
-        return (
-              <div className="container">
-                  <div className="col-sm-8 col-sm-offset-2">
-                              <LoginPage />
-                  </div>
-              </div>
-        );
-    }
+  render() {
+    const { alert } = this.props;
+    return (
+      <div className="container">
+        <Router>
+          <div className="col-sm-8 col-sm-offset-2">
+            <Route exact path="/" component={HomePage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/register" component={RegisterPage} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    const { alert } = state;
-    return {
-        alert
-    };
+  const { alert } = state;
+  return {
+    alert
+  };
 }
